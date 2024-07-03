04cbdf99d1e4b3048c2e9a560f1f5387cc00047f
chahyoil <chahyoil@gmail.com>
2024-07-03 14:40:40 +0900

    edit : factories에서는 DB 작업X. 목킹할 데이터만 생성하도록 수정

--- app/store_system/tests/factories.py
+++ app/store_system/tests/factories.py
@@ -1,36 +1,31 @@
 import factory
 from factory.faker import Faker
 from app.store_system import models
-from app.core.database import SessionLocal
 
-class CustomerFactory(factory.alchemy.SQLAlchemyModelFactory):
+class CustomerFactory(factory.Factory):
     class Meta:
         model = models.Customer
-        sqlalchemy_session = SessionLocal()
 
     name = Faker('name')
     email = Faker('email')
 
-class ProductFactory(factory.alchemy.SQLAlchemyModelFactory):
+class ProductFactory(factory.Factory):
     class Meta:
         model = models.Product
-        sqlalchemy_session = SessionLocal()
 
     name = Faker('word')
     price = Faker('pyfloat', left_digits=2, right_digits=2, positive=True)
 
-class StoreFactory(factory.alchemy.SQLAlchemyModelFactory):
+class StoreFactory(factory.Factory):
     class Meta:
         model = models.Store
-        sqlalchemy_session = SessionLocal()
 
     name = Faker('company')
     location = Faker('address')
 
-class PurchaseFactory(factory.alchemy.SQLAlchemyModelFactory):
+class PurchaseFactory(factory.Factory):
     class Meta:
         model = models.Purchase
-        sqlalchemy_session = SessionLocal()
 
     customer = factory.SubFactory(CustomerFactory)
     product = factory.SubFactory(ProductFactory)
